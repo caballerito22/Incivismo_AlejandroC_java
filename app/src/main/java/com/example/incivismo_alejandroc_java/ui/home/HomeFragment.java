b0f452e410254c26d0565e341899bb6e2582dc61
alecabnun <avestruz2265@gmail.com>
2024-12-18 13:26:23 +0100

    LOCATION LISTO

--- app/src/main/java/com/example/incivismo_alejandroc_java/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/incivismo_alejandroc_java/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.incivismo_alejandroc_java.ui.home;
 
+
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +27,9 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.incivismo_alejandroc_java.databinding.FragmentHomeBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
 import java.io.IOException;
@@ -36,20 +41,21 @@ import java.util.concurrent.Executors;
 
 public class HomeFragment extends Fragment {
 
-    private ActivityResultLauncher <String[]> locationPermissionRequest;
+    private ActivityResultLauncher<String[]> locationPermissionRequest;
+
     private FragmentHomeBinding binding;
+
     private Location mLastLocation;
     private FusedLocationProviderClient mFusedLocationClient;
 
-    //lo pongo para que el locationPermissionRequest de getLocation no de error
-    //ActivityResultLauncher<String[]> locationPermissionRequest = null;
+    private boolean mTrackingLocation;
+    private LocationCallback mLocationCallback;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
@@ -57,6 +63,7 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.localitzacio;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         locationPermissionRequest = registerForActivityResult(new ActivityResultContracts
                         .RequestMultiplePermissions(), result -> {
                     Boolean fineLocationGranted = result.getOrDefault(
@@ -72,7 +79,56 @@ public class HomeFragment extends Fragment {
                     }
                 }
         );
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (mTrackingLocation) {
+                    fetchAddress(locationResult.getLastLocation());
+                }
+            }
+        };
+
+        binding.buttonLocation.setOnClickListener((View clickedView) -> {
+            if (!mTrackingLocation) {
+                startTrackingLocation();
+            } else {
+                stopTrackingLocation();
+            }
+        });
+
         return root;
+
+    }
+
+    private void startTrackingLocation() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(requireContext(), "Request permisssions", Toast.LENGTH_SHORT).show();
+            locationPermissionRequest.launch(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION});
+        } else {
+            Toast.makeText(requireContext(), "getLocation: permissions granted", Toast.LENGTH_SHORT).show();
+            mFusedLocationClient.requestLocationUpdates(getLocationRequest(), mLocationCallback, null);
+        }
+        binding.localitzacio.setText("Carregant...");
+        binding.localitzacio.setVisibility(ProgressBar.VISIBLE);
+        mTrackingLocation = true;
+        binding.buttonLocation.setText("Aturar el seguiment de la ubicació");
+    }
+
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    private void stopTrackingLocation() {
+        if (mTrackingLocation) {
+            binding.localitzacio.setVisibility(ProgressBar.INVISIBLE);
+            mTrackingLocation = false;
+            binding.buttonLocation.setText("Comença a seguir la ubicació");
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
     }
 
     private void getLocation() {
@@ -86,17 +142,21 @@ public class HomeFragment extends Fragment {
                     Manifest.permission.ACCESS_COARSE_LOCATION
             });
         } else {
-            Toast.makeText(requireContext(), "getLocation: permissions granted", Toast.LENGTH_SHORT).show();
             mFusedLocationClient.getLastLocation().addOnSuccessListener(
                     location -> {
                         if (location != null) {
-                            fetchAddress(location);
+                            mLastLocation = location;
+                            binding.localitzacio.setText(
+                                    String.format("Latitud: %1$.4f \n Longitud: %2$.4f\n Hora: %3$tr",
+                                            mLastLocation.getLatitude(),
+                                            mLastLocation.getLongitude(),
+                                            mLastLocation.getTime()));
                         } else {
                             binding.localitzacio.setText("Sense localització coneguda");
                         }
                     });
         }
-        binding.localitzacio.setText("Carregant...");
+
     }
 
     private void fetchAddress(Location location) {
@@ -107,7 +167,7 @@ public class HomeFragment extends Fragment {
                 Locale.getDefault());
 
         executor.execute(() -> {
-            // Aquest codi s'executa en segon pla
+
             List<Address> addresses = null;
             String resultMessage = "";
 
