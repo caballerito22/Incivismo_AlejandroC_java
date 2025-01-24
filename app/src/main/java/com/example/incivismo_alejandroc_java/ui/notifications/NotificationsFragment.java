ce2340a7b8e2a71a255137a559a90bc110bad18a
caballerito22 <alecabnun@alu.edu.gva.es>
2025-01-24 11:38:47 +0100

    4.3.1 hecho, falta poner la última localización

--- app/src/main/java/com/example/incivismo_alejandroc_java/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/incivismo_alejandroc_java/ui/notifications/NotificationsFragment.java
@@ -1,6 +1,8 @@
 package com.example.incivismo_alejandroc_java.ui.notifications;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,15 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.incivismo_alejandroc_java.databinding.FragmentNotificationsBinding;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class NotificationsFragment extends Fragment {
 
@@ -24,9 +35,47 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Context ctx = requireActivity().getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        binding.map.setTileSource(TileSourceFactory.MAPNIK);
+        binding.map.setMultiTouchControls(true);
+        IMapController mapController = binding.map.getController();
+        mapController.setZoom(14.5);
+        GeoPoint startPoint = new GeoPoint(39.4715612, -0.3930977);
+        mapController.setCenter(startPoint);
+
+        MyLocationNewOverlay myLocationNewOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(requireContext()), binding.map);
+        myLocationNewOverlay.enableMyLocation();
+        binding.map.getOverlays().add(myLocationNewOverlay);
+
+        CompassOverlay compassOverlay = new CompassOverlay(requireContext(), new InternalCompassOrientationProvider(requireContext()), binding.map);
+        compassOverlay.enableCompass();
+        binding.map.getOverlays().add(compassOverlay);
+
         return root;
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        binding.map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        binding.map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 
     @Override
